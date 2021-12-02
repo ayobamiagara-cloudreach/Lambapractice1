1fa3c21130edc77ff53b28d6092677a4faf8e0dc
AyobamiAgara <ayobami.agara@cloudreach.com>
2021-12-02 13:41:27 +0000

    Delete the Lambadascript.zip

--- lambdascript.py
+++ lambdascript.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 def handler(event, context):
-    s3_obj =boto3.client('s3',aws_access_key_id="AKIASW2BROLCV6KN22UA",aws_secret_access_key="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") #creating object for accessing s3
+    s3_obj =boto3.client('s3')    #creating object for accessing s3  
 
     s3_pet_obj = s3_obj.get_object(Bucket='newbucket-lambda-2021', Key='Petinfo.json')
     s3__pet_data = s3_pet_obj['Body'].read().decode('utf-8')
